f3155ad64294f922b8282b3606282653966b21b0
Nursat2022 <nursatsakyshev2003@gmail.com>
2023-05-07 19:08:52 +0600

    UI: stroke

--- PomodoroApp/PomodoroApp/ViewController.swift
+++ PomodoroApp/PomodoroApp/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         setupUI()
     }
-
+    
     func setupUI() {
         view.addSubview(image)
         let configuration = UIImage.SymbolConfiguration(weight: .black)
@@ -35,6 +35,44 @@ class ViewController: UIViewController {
             button.centerXAnchor.constraint(equalTo: view.centerXAnchor),
             button.topAnchor.constraint(equalTo: view.topAnchor, constant: 148),
         ])
+        
+        let circleView = CircleStrokeView(frame: CGRect(x: 0, y: 0, width: 248, height: 248))
+        view.addSubview(circleView)
+        
+        circleView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            circleView.widthAnchor.constraint(equalToConstant: 248),
+            circleView.heightAnchor.constraint(equalToConstant: 248),
+            circleView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            circleView.topAnchor.constraint(equalTo: button.bottomAnchor, constant: 52)
+        ])
     }
 }
 
+class CircleStrokeView: UIView {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        backgroundColor = .clear
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        backgroundColor = .clear
+    }
+
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+
+        let center = CGPoint(x: rect.midX, y: rect.midY)
+        let radius = rect.height / 2 - 5
+
+        let path = UIBezierPath()
+        path.addArc(withCenter: center, radius: radius, startAngle: 0, endAngle: CGFloat.pi * 2, clockwise: true)
+
+        UIColor(red: 1, green: 1, blue: 1, alpha: 0.3).setStroke()
+        path.lineWidth = 6.0
+        path.lineCapStyle = .round
+
+        path.stroke()
+    }
+}
