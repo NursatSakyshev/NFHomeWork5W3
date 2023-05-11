b00eef2128d972a1237bdfa066115d1c2691bc52
Nursat2022 <nursatsakyshev2003@gmail.com>
2023-05-11 17:34:28 +0600

    home page

--- PomodoroApp/PomodoroApp/ViewController.swift
+++ PomodoroApp/PomodoroApp/ViewController.swift
@@ -7,14 +7,21 @@
 
 import UIKit
 
+enum mode {
+    case Focus
+    case Break
+}
+
 class ViewController: UIViewController {
     
     let image = UIImageView(image: UIImage(named: "BG"))
     let settings = Settings()
     lazy var date: Date = settings.focusTime
-    var isPlaying = true
+    var isPlaying = false
     let button = UIButton()
+    var timerMode: mode = .Focus
     var timer: Timer?
+    var today: String
     let playButton = UIButton()
     let stopButton = UIButton()
     let stackView = UIStackView()
@@ -26,7 +33,25 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         setupUI()
-        updateTimer()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        date = settings.focusTime
+        timeLabel.text = date.toString()
+    }
+    
+    init() {
+        today = Date().getDay()
+        var arr: [String] = UserDefaults.standard.object(forKey: "days") as? [String] ?? []
+        if !arr.contains(today) {
+            arr.append(today)
+        }
+        UserDefaults.standard.setValue(arr, forKey: "days")
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
     
     func calculatePercentage(bigDate: Date, smallDate: Date) -> CGFloat {
@@ -38,12 +63,34 @@ class ViewController: UIViewController {
             guard let self = self else { return }
             self.date = self.date.addingTimeInterval(-1)
             self.timeLabel.text = self.date.toString()
-            self.circleProgressView.setProgress(self.calculatePercentage(bigDate: self.settings.focusTime, smallDate: self.date))
+            self.circleProgressView.setProgress(self.calculatePercentage(bigDate: self.timerMode == .Focus ? self.settings.focusTime : self.settings.breakTime, smallDate: self.date))
+            self.check()
         }
     }
-       func stopTimer() {
-           timer?.invalidate()
-       }
+    
+    func check() {
+        if date.getSeconds() < 1 {
+            var history = UserDefaults.standard.object(forKey: today) as? [String: Int] ?? ["Focus time": 0, "Break time": 0]
+            switch timerMode {
+            case .Focus:
+                history["Focus time"]! += settings.focusTime.getSeconds()
+                UserDefaults.standard.setValue(history, forKey: today)
+                date = settings.breakTime
+                timerMode = .Break
+                motivationLabel.text = "Break"
+            case .Break:
+                history["Break time"]! += settings.breakTime.getSeconds()
+                UserDefaults.standard.setValue(history, forKey: today)
+                date = settings.focusTime
+                timerMode = .Focus
+                motivationLabel.text = "Focus on your task"
+            }
+        }
+    }
+    
+    func stopTimer() {
+        timer?.invalidate()
+    }
     
     func setupUI() {
         [button, playButton, stopButton, stackView, motivationLabel, timeLabel, circleView, circleProgressView].forEach {
@@ -78,10 +125,11 @@ class ViewController: UIViewController {
         motivationLabel.font = UIFont.systemFont(ofSize: 16)
         motivationLabel.textAlignment = .center
         
-        playStopButtonConfiguration(playButton, imageName: "pause", pointSize: 25)
+        playStopButtonConfiguration(playButton, imageName: isPlaying ? "pause" : "play", pointSize: 25)
         playButton.addTarget(self, action: #selector(playButtonPressed), for: .touchUpInside)
         
         playStopButtonConfiguration(stopButton, imageName: "stop.fill", pointSize: 20)
+        stopButton.addTarget(self, action: #selector(stopButtonPressed), for: .touchUpInside)
         
         stackView.addArrangedSubview(playButton)
         stackView.addArrangedSubview(stopButton)
@@ -141,38 +189,57 @@ class ViewController: UIViewController {
         playButton.setImage(image, for: .normal)
         isPlaying ? updateTimer() : stopTimer()
     }
+    
+    @objc func stopButtonPressed() {
+        var historyToday: [String: Int] = UserDefaults.standard.object(forKey: today) as? [String: Int] ?? ["Focus time": 0, "Break time": 0]
+        switch timerMode {
+        case .Focus:
+            historyToday["Focus time"]! += settings.focusTime.getSeconds() - date.getSeconds()
+            date = settings.focusTime
+        
+        case .Break:
+            historyToday["Break time"]! += settings.breakTime.getSeconds() - date.getSeconds()
+            date = settings.breakTime
+        }
+        UserDefaults.standard.setValue(historyToday, forKey: today)
+//        isPlaying = false
+    }
+    
+    
 }
+
+//MARK: CIRCLE PROGRESS VIEW
 
 class CircleProgressView: UIView {
     private var progressLayer: CAShapeLayer!
-
+    
     override init(frame: CGRect) {
         super.init(frame: frame)
         setupProgressLayer()
     }
-
+    
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         setupProgressLayer()
     }
-
+    
     private func setupProgressLayer() {
         let center = CGPoint(x: bounds.midX, y: bounds.midY)
         let radius = min(bounds.width, bounds.height) / 2.0 - 5
         let startAngle = -CGFloat.pi / 2.0
         let endAngle = startAngle + (2.0 * CGFloat.pi)
         let circularPath = UIBezierPath(arcCenter: center, radius: radius, startAngle: startAngle, endAngle: endAngle, clockwise: true)
-
+        
         progressLayer = CAShapeLayer()
         progressLayer.path = circularPath.cgPath
-//        progressLayer.strokeColor = UIColor.white.cgColor
+        //        progressLayer.strokeColor = UIColor.white.cgColor
         progressLayer.lineWidth = 6.0
         progressLayer.fillColor = UIColor.clear.cgColor
         progressLayer.strokeEnd = 0.0  // Initial progress
-
+        
         layer.addSublayer(progressLayer)
     }
-
+    
     func setProgress(_ progress: CGFloat) {
         progressLayer.strokeEnd = progress
         let strokeColor = progress == 1 ? UIColor(red: 1, green: 1, blue: 1, alpha: 0.3) : UIColor.white
@@ -180,6 +247,7 @@ class CircleProgressView: UIView {
     }
 }
 
+// MARK: EXTENSION DATE
 extension Date {
     func getSeconds() -> Int {
         let calendar = Calendar.current
@@ -205,7 +273,7 @@ extension Date {
     }
 }
 
-
+// MARK: EXTENSION INT
 extension Int {
     func toDate() -> String {
         let hours = self / 3600
