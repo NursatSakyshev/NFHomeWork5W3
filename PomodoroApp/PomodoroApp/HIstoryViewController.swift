a92d0a0f7255f1129dd5308fbf04785b5612c427
Nursat2022 <nursatsakyshev2003@gmail.com>
2023-05-12 14:55:11 +0600

    history

--- PomodoroApp/PomodoroApp/HIstoryViewController.swift
+++ PomodoroApp/PomodoroApp/HIstoryViewController.swift
@@ -9,25 +9,24 @@ import UIKit
 
 class HIstoryViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
-    var date = ["12.11.12", "21.23.34", "12.12.12"]
-    
-    var settingsDate = ["12.11.12" : ["Focus time": "20", "Break time" : "50"], "21.23.34": ["Focus time": "30", "Break time" : "40"], "12.12.12": ["Focus time": "12", "Break time" : "340"]]
+    var days: [String] {
+        UserDefaults.standard.object(forKey: "days") as? [String] ?? []
+    }
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return date.count
+        return days.count
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 2
     }
     
-    
-    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! HistoryCell
         cell.timeMode.text = indexPath.row == 0 ? "Focus time" : "Break time"
-        let today = date[indexPath.section]
-        cell.time.text = indexPath.row == 0 ? settingsDate[today]!["Focus time"] : settingsDate[today]!["Break time"]
+        let day = days[indexPath.section]
+        var history = (UserDefaults.standard.object(forKey: day) as? [String: Int] ?? ["Focus time": 0, "Break time": 0])
+        cell.time.text = indexPath.row == 0 ? history["Focus time"]?.toDate() : history["Break time"]?.toDate()
         cell.backgroundColor = UIColor(red: 28/255, green: 28/255, blue: 30/255, alpha: 1)
         cell.isUserInteractionEnabled = false
         return cell
@@ -37,6 +36,9 @@ class HIstoryViewController: UIViewController, UITableViewDelegate, UITableViewD
         54
     }
     
+    override func viewWillAppear(_ animated: Bool) {
+        tableView.reloadData()
+    }
     
     let label = UILabel()
     let tableView = UITableView()
@@ -76,7 +78,7 @@ class HIstoryViewController: UIViewController, UITableViewDelegate, UITableViewD
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
         let headerView = UIView()
         let sectionContentView = UILabel()
-        sectionContentView.text = date[section]
+        sectionContentView.text = days[section]
         sectionContentView.textColor = .white
         sectionContentView.font = UIFont.systemFont(ofSize: 20, weight: .bold)
         sectionContentView.translatesAutoresizingMaskIntoConstraints = false
@@ -101,7 +103,6 @@ class HistoryCell: UITableViewCell {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         timeMode.textColor = .white
         time.textColor = UIColor(red: 235/255, green: 235/255, blue: 245/255, alpha: 0.6)
-        time.text = "hello"
         
         [timeMode, time].forEach {
             $0.translatesAutoresizingMaskIntoConstraints = false
